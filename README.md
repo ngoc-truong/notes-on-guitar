9f77b7844dea55e002b53506dc07768ae7600f2e
Ngoc Truong <ngoc.truong@posteo.de>
2020-04-28 01:40:47 +0200

    Finish restart after all notes found

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ This app is built with HTML, CSS, and Javascript.
 ## Further similar app ideas
 - The app will show all notes in a particular chord, so that users can design their own chord shapes.
 - The app will show a (major) key and the user has to enter all notes in this key.
+
+## Bug fixes
+- Implement refresh button
+- If all notes were found, fretboard should refresh
+- Nice to have: Counter of which note was practiced how much
--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const string3 = createNotesOnString(allNotes, 11);
 const string4 = createNotesOnString(allNotes, 6);
 const string5 = createNotesOnString(allNotes, 1);
 const string6 = createNotesOnString(allNotes, 8);
+let correct = 0;
+let incorrect = 0;
 
 
 // DOM Manipulation
@@ -19,9 +21,10 @@ const fretsString3  = document.querySelectorAll(".string-3");
 const fretsString4  = document.querySelectorAll(".string-4");
 const fretsString5  = document.querySelectorAll(".string-5");
 const fretsString6  = document.querySelectorAll(".string-6");
-
 const allFrets      = document.querySelectorAll(".fret");
 const targetNote    = document.querySelector("#target");
+const numCorrect    = document.querySelector("#num-correct");
+const numWrong      = document.querySelector("#num-wrong");
 
 setNotesOnDivs(fretsString1, string1);
 setNotesOnDivs(fretsString2, string2);
@@ -29,6 +32,7 @@ setNotesOnDivs(fretsString3, string3);
 setNotesOnDivs(fretsString4, string4);
 setNotesOnDivs(fretsString5, string5);
 setNotesOnDivs(fretsString6, string6);
+
 
 function createFretboard(){
     for (let string = 1; string <= 6; string++) {
@@ -71,14 +75,47 @@ function createRandomNote(notes){
     return notes[position];
 };
 
+function restart() {
+    let correctAnswers = document.querySelectorAll(".correct");
+    let wrongAnswers = document.querySelectorAll(".wrong");
+
+    correctAnswers.forEach( (answer) => {
+        answer.classList.remove("correct");
+    })
+
+    wrongAnswers.forEach( (answer) => {
+        answer.classList.remove("wrong");
+    })
+
+    correct = 0;
+    incorrect = 0;
+
+    numCorrect.textContent = "Correct Answers: 0";
+    numWrong.textContent = "Wrong Answers: 0";
+    targetNote.textContent = createRandomNote(allNotes);
+}
+
 targetNote.textContent = createRandomNote(allNotes);
 
 allFrets.forEach( (fret) => {
     fret.addEventListener("click", (e) => {
+
         if (targetNote.textContent === fret.value){
-            fret.classList.add("correct");
+            if (!fret.classList.contains("correct")){
+                fret.classList.add("correct");
+                correct++;
+            }
+            numCorrect.textContent = `Correct answers: ${correct}`;
         } else {
-            fret.classList.add("wrong");
+            if (!fret.classList.contains("wrong")){
+                fret.classList.add("wrong");
+                incorrect++;
+            }
+            numWrong.textContent = `Wrong answers: ${incorrect}`;
+        }
+
+        if (correct === 6) {
+            restart(); 
         }
     });
 });
@@ -94,8 +131,5 @@ createDot(fretsString1, 11, "b");
 -   Refactor: Instead of repetitive code (e.g. setNotesOnDivs(fretsString1, string1), 
     setNotesOnDivs(fretsString2, string2...), use (window['value' + i]), see
     https://www.geeksforgeeks.org/how-to-use-dynamic-variable-names-in-javascript/
--   How many correct and incorrect answers?
--   How many notes left?
--   
 
 */
